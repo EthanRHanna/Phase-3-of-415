f62ec75ac1e802369f4c7eb9add1ad7097866e81
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2023-04-30 20:58:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const express = require("express");
 const { MongoClient } = require("mongodb");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
-const { error } = require("console");
-const xmlparser = require("express-xml-bodyparser");
-const { xml2js } = require("xml-js");
-
 var fs = require("fs");
 var js2xmlparser = require("js2xmlparser");
-
+const { error } = require("console");
 app.listen(port);
 app.use(express.json());
-app.use(xmlparser);
 app.use(express.urlencoded({ extended: true }));
-
 const uri =
   "mongodb+srv://User:HPIx5GGvfwzjgGNF@cluster0.cllmezs.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
+
+var updateId = -1;
 
 app.get("/form", function (req, res) {
   res.setHeader("Content-Type", "text/html");
@@ -182,43 +180,7 @@ app.get("/xml/ticket/:id", function (req, res) {
 
     res.send(js2xmlparser.parse("Ticket", result)).status(200);
   }
+  //Hi
 
   run().catch(console.log(error));
-});
-
-app.patch("/xml/patch/:id", function (req, res) {
-  const client = new MongoClient(uri);
-
-  async function run() {
-    try {
-      const database = client.db("cluster0");
-      const ticket = database.collection("SampleForProject");
-      const searchId = req.params.id;
-      const query = { id: searchId };
-
-      var updateTicket = {
-        $set: {
-          createdAt: req.body.createdAt,
-          updatedAt: req.body.updatedAt,
-          type: req.body.type,
-          subject: req.body.subject,
-          Description: req.body.Description,
-          priority: req.body.priority,
-          status: req.body.status,
-          recipient: req.body.recipient,
-          submitter: req.body.submitter,
-          assignee_ID: req.body.assignee_ID,
-          follower_IDs: req.body.follower_IDs,
-          tags: req.body.tags,
-        },
-      };
-      await ticket.updateOne(query, updateTicket);
-      let result = await ticket.findOne(query);
-      console.log(ticket);
-      res.send(result).status(200);
-    } finally {
-      await client.close();
-    }
-  }
-  run().catch(console.dir);
 });
