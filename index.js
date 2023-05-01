a0c3cdc5c6f9f272d81226f2f49a6b636faa00b4
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2023-04-30 21:29:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -185,3 +185,42 @@ app.get("/rest/xml/ticket/:id", function (req, res) {
 
   run().catch(console.log(error));
 });
+
+//XML Patch Method
+app.patch("/rest/xml/patch/:id", function (req, res) {
+  const client = new MongoClient(uri);
+
+  async function run() {
+    try {
+      const database = client.db("cluster0");
+      const ticket = database.collection("SampleForProject");
+      const searchId = req.params.id;
+      const query = { id: searchId };
+
+      var updateTicket = {
+        $set: {
+          createdAt: req.body.createdAt,
+          updatedAt: req.body.updatedAt,
+          type: req.body.type,
+          subject: req.body.subject,
+          Description: req.body.Description,
+          priority: req.body.priority,
+          status: req.body.status,
+          recipient: req.body.recipient,
+          submitter: req.body.submitter,
+          assignee_ID: req.body.assignee_ID,
+          follower_IDs: req.body.follower_IDs,
+          tags: req.body.tags,
+        },
+      };
+
+      await ticket.updateOne(query, updateTicket);
+      let result = await ticket.findOne(query);
+      console.log(ticket);
+      res.send(result).status(200);
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+});
