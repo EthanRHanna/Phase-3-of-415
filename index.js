238f57cc1c13dd0a2f21b8f58308bf51e49634ec
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2023-04-30 19:01:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ const port = 3000;
 var fs = require("fs");
 var js2xmlparser = require("js2xmlparser");
 const { error } = require("console");
+const xmlparser = require("express-xml-bodyparser");
+const { xml2js } = require("xml-js");
 app.listen(port);
 app.use(express.json());
+app.use(bodyParser);
 app.use(express.urlencoded({ extended: true }));
 const uri =
   "mongodb+srv://User:HPIx5GGvfwzjgGNF@cluster0.cllmezs.mongodb.net/?retryWrites=true&w=majority";
@@ -183,3 +186,44 @@ app.get("/xml/ticket/:id", function (req, res) {
 
   run().catch(console.log(error));
 });
+
+app.patch(
+  "/xml/patch/:id",
+  xmlparser({ trim: false, explicitArray: false }),
+  function (req, res) {
+    const client = new MongoClient(uri);
+
+    async function run() {
+      try {
+        const database = client.db("cluster0");
+        const ticket = database.collection("SampleForProject");
+        const searchId = req.params.id;
+        const query = { id: searchId };
+
+        var updateTicket = {
+          $set: {
+            createdAt: req.body.createdAt,
+            updatedAt: req.body.updatedAt,
+            type: req.body.type,
+            subject: req.body.subject,
+            Description: req.body.Description,
+            priority: req.body.priority,
+            status: req.body.status,
+            recipient: req.body.recipient,
+            submitter: req.body.submitter,
+            assignee_ID: req.body.assignee_ID,
+            follower_IDs: req.body.follower_IDs,
+            tags: req.body.tags,
+          },
+        };
+        await ticket.updateOne(query, JSON.stringify(xml2js(updateTicket)));
+        let result = await ticket.findOne(query);
+        console.log(ticket);
+        res.send(result).status(200);
+      } finally {
+        await client.close();
+      }
+    }
+    run().catch(console.dir);
+  }
+);
